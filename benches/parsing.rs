d333e5b718d278522c7abd20a0d04652eaa14159
jiyinyiyong <jiyinyiyong@gmail.com>
2021-10-31 12:37:03 +0800

    add bench demo; tag 0.1.12

---
+++ benches/parsing.rs
@@ -0,0 +1,18 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::fs;
+
+use cirru_parser::parse;
+
+fn criterion_benchmark(c: &mut Criterion) {
+  let large_demo = "/Users/chen/repo/calcit-lang/editor/compact.cirru";
+  let content = fs::read_to_string(large_demo).unwrap();
+
+  c.bench_function("parse", |b| {
+    b.iter(|| {
+      let _ = parse(&content);
+    })
+  });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
